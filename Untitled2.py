7dcab0dde166b7e700e338ad2f99c2199a36b16f
braddatatrained <110330382+braddatatrained@users.noreply.github.com>
2022-07-31 15:57:26 +0530

    Add files via upload

---
+++ Untitled2.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+pd.read_csv(r"https://raw.githubusercontent.com/dsrscientist/DSData/master/Advertising.csv")
+
+
+# In[3]:
+
+
+import os
+
+
+# In[4]:
+
+
+os.getcwd()
+
+
+# In[5]:
+
+
+pd.read_csv(r"student.tsv")
+
+
+# In[ ]:
+
+
+
+
